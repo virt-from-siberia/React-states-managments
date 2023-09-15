1387650168a2934a3f784713d785e7dac233063b
Aleksey Elchin <alonso85@mail.ru>
2023-09-16 04:09:11 +0900

    RTK Query 7 lesson

--- React Redux Toolkit Tutorial for Beginners  Learn Modern Redux/7 RTK Query Avanced/src/features/posts/PostsList.js
+++ React Redux Toolkit Tutorial for Beginners  Learn Modern Redux/7 RTK Query Avanced/src/features/posts/PostsList.js
@@ -1,26 +1,25 @@
 import { useSelector } from "react-redux";
-import { selectPostIds, getPostsStatus, getPostsError } from "./postsSlice";
+import { selectPostIds } from "./postsSlice";
 import PostsExcerpt from "./PostsExcerpt";
+import { useGetPostsQuery } from "./postsSlice";
 
 const PostsList = () => {
+  const { isLoading, isSuccess, isError, error } = useGetPostsQuery();
+  const orderedPostIds = useSelector(selectPostIds);
+  //   const postStatus = useSelector(getPostsStatus);
+  //   const error = useSelector(getPostsError);
 
-    const orderedPostIds = useSelector(selectPostIds)
-    const postStatus = useSelector(getPostsStatus);
-    const error = useSelector(getPostsError);
+  let content;
+  if (isLoading) {
+    content = <p>"Loading..."</p>;
+  } else if (isSuccess) {
+    content = orderedPostIds.map((postId) => (
+      <PostsExcerpt key={postId} postId={postId} />
+    ));
+  } else if (isError) {
+    content = <p>{error}</p>;
+  }
 
-    let content;
-    if (postStatus === 'loading') {
-        content = <p>"Loading..."</p>;
-    } else if (postStatus === 'succeeded') {
-        content = orderedPostIds.map(postId => <PostsExcerpt key={postId} postId={postId} />)
-    } else if (postStatus === 'failed') {
-        content = <p>{error}</p>;
-    }
-
-    return (
-        <section>
-            {content}
-        </section>
-    )
-}
-export default PostsList
+  return <section>{content}</section>;
+};
+export default PostsList;
