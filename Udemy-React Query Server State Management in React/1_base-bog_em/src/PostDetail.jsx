86f9f3cbd4ef3cac0bba39891ad7c33b98a59100
Aleksey Elchin <alonso85@mail.ru>
2023-12-22 06:32:15 +0900

    react-query

--- Udemy-React Query Server State Management in React/1_base-bog_em/src/PostDetail.jsx
+++ Udemy-React Query Server State Management in React/1_base-bog_em/src/PostDetail.jsx
@@ -34,6 +34,8 @@ export function PostDetail({ post }) {
 
   const deleteMutation = useMutation((postId) => deletePost(postId));
 
+  const updateMutation = useMutation((postId) => updatePost(postId));
+
   if (isLoading) return <div>loading...</div>;
 
   if (isError)
@@ -43,6 +45,7 @@ export function PostDetail({ post }) {
     <>
       <h3 style={{ color: "blue" }}>{post.title}</h3>
       <button onClick={() => refetch()}>Refresh Post</button>
+
       <button onClick={() => deleteMutation.mutate(post.id)}>Delete</button>
       {deleteMutation.isError && (
         <p style={{ color: "red" }}>Error deleting the post</p>
@@ -53,7 +56,20 @@ export function PostDetail({ post }) {
       {deleteMutation.isSuccess && (
         <p style={{ color: "green" }}>Post has been deleted</p>
       )}
-      <button>Update title</button>
+
+      <button onClick={() => updateMutation.mutate(post.id)}>
+        Update title
+      </button>
+      {updateMutation.isError && (
+        <p style={{ color: "red" }}>Error updating the post</p>
+      )}
+      {updateMutation.isLoading && (
+        <p style={{ color: "purple" }}>Updating the post</p>
+      )}
+      {updateMutation.isSuccess && (
+        <p style={{ color: "green" }}>Post has been updated</p>
+      )}
+
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data.map((comment) => (
