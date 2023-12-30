7f3822d5a3e959f27db68b1201e80edb7ca6e0e0
Aleksey Elchin <alonso85@mail.ru>
2023-12-31 04:09:12 +0900

    react-query

--- Udemy-React Query Server State Management in React/3_base-lazy-days/client/src/components/appointments/hooks/useCancelAppointment.ts
+++ Udemy-React Query Server State Management in React/3_base-lazy-days/client/src/components/appointments/hooks/useCancelAppointment.ts
@@ -1,22 +1,40 @@
+/* eslint-disable simple-import-sort/imports */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { UseMutateFunction, useMutation, useQueryClient } from 'react-query';
 import { Appointment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
 import { useCustomToast } from '../../app/hooks/useCustomToast';
+import { useUser } from '../../user/hooks/useUser';
 
 // for when server call is needed
-// async function removeAppointmentUser(appointment: Appointment): Promise<void> {
-//   const patchData = [{ op: 'remove', path: '/userId' }];
-//   await axiosInstance.patch(`/appointment/${appointment.id}`, {
-//     data: patchData,
-//   });
-// }
+async function removeAppointmentUser(appointment: Appointment): Promise<void> {
+  const patchData = [{ op: 'remove', path: '/userId' }];
+  await axiosInstance.patch(`/appointment/${appointment.id}`, {
+    data: patchData,
+  });
+}
 
-// TODO: update return type
-export function useCancelAppointment(): (appointment: Appointment) => void {
+export function useCancelAppointment(): UseMutateFunction<
+  void, // returned
+  unknown, // error
+  Appointment, // passed args to mutation function
+  unknown // context type
+> {
   const toast = useCustomToast();
+  const queryClient = useQueryClient();
 
-  // TODO: replace with mutate function
-  return (appointment: Appointment) => {
-    // nothing to see here
-  };
+  const { mutate } = useMutation(removeAppointmentUser, {
+    onSuccess: () => {
+      queryClient.invalidateQueries([queryKeys.appointments]);
+      toast({
+        title: 'appointment canceled',
+        status: 'success',
+        duration: 5000,
+        isClosable: true,
+      });
+    },
+  });
+
+  return mutate;
 }
